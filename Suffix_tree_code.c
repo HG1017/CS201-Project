5dba38a308448d68e7b3c7c0a084c7cc6fb0b9c9
HG1017 <94758255+HG1017@users.noreply.github.com>
2021-11-21 19:40:24 +0530

    Create Suffix_tree_code.c

---
+++ Suffix_tree_code.c
@@ -0,0 +1,10 @@
+// a C program for the implementation of the construction of suffix tree from a
+// given string input and also printing the linear time suffix array for the same.
+
+/* Team details:-
+    Group number 35
+    Members:-
+    1) Shivam Choubey (2019med1006)
+    2) Harsh Gupta (2019med1008)
+*/
+
